6687cc02a25a43ed532a71c07448fac36cc5ddf2
Makuss88 <maciborek.grzegorz@gmail.com>
2023-05-09 10:02:34 +0200

    add svg, and theme.ts

---
+++ movie-tinder/src/static/icons.ts
@@ -0,0 +1,7 @@
+import acceptIcon from "./svg/acceptIcon.svg";
+import rejectIcon from "./svg/rejectIcon.svg";
+
+export const icons = {
+  acceptIcon,
+  rejectIcon,
+};
--- movie-tinder/src/static/theme.ts
+++ movie-tinder/src/static/theme.ts
@@ -1,88 +1,46 @@
-
 export const colors = {
   normal: {
-    black: '#0F0F1A',
-    white: '#F7F7F7',
-    greyText: '#707A8D',
-    red: '#D25858',
-    green: '#31ce56',
+    black: "#0F0F1A",
+    white: "#F7F7F7",
+    greyText: "#707A8D",
+    red: "#D25858",
+    green: "#31ce56",
   },
   hover: {
-    red: '#c43535',
-    green: '#83b20c'
-  }
-}
+    red: "#c43535",
+    green: "#83b20c",
+  },
+};
 
 export const typography = {
   heading1: {
-    fontSize: 36,
-    lineHeight: '54px',
-    fontWeight: 500
+    fontSize: 88,
+    lineHeight: "66px",
+    fontWeight: 700,
   },
   heading2: {
-    fontSize: 32,
-    lineHeight: '48px',
-    fontWeight: 500
+    fontSize: 60,
+    lineHeight: "60px",
+    fontWeight: 500,
   },
   heading3: {
-    fontSize: 28,
-    lineHeight: '42px',
-    fontWeight: 500
+    fontSize: 40,
+    lineHeight: "42px",
+    fontWeight: 500,
   },
   heading4: {
-    fontSize: 24,
-    lineHeight: '36px',
-    fontWeight: 500
+    fontSize: 32,
+    lineHeight: "36px",
+    fontWeight: 300,
   },
   heading5: {
-    fontSize: 30,
-    lineHeight: '45px',
-    fontWeight: 500
+    fontSize: 20,
+    lineHeight: "45px",
+    fontWeight: 500,
   },
   heading6: {
-    fontSize: 18,
-    lineHeight: '24px',
-    fontWeight: 300
-  },
-
-  subtitle: {
-    fontSize: 20,
-    lineHeight: '30px',
-    fontWeight: 500,
-    letterSpacing: '0.02em'
-  },
-  body1: {
     fontSize: 16,
-    lineHeight: '24px',
-    fontWeight: 500,
-    letterSpacing: '0.02em'
-  },
-  body2: {
-    fontSize: 16,
-    lineHeight: '24px',
+    lineHeight: "24px",
     fontWeight: 300,
-    letterSpacing: '0.02em'
   },
-  body3: {
-    fontSize: 14,
-    lineHeight: '21px',
-    fontWeight: 500
-  },
-  body4: {
-    fontSize: 14,
-    lineHeight: '21px',
-    fontWeight: 300,
-    letterSpacing: '0.02em'
-  },
-  body5: {
-    fontSize: 14,
-    lineHeight: '18px',
-    fontWeight: 300,
-    letterSpacing: '0.02em'
-  },
-  body6: {
-    fontSize: 16,
-    lineHeight: '21px',
-    fontWeight: 300
-  },
-}
+};
