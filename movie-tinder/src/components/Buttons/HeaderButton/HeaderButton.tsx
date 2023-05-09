e00c642aff36e4a3af7cd64b3f1b425c78663c6a
Makuss88 <maciborek.grzegorz@gmail.com>
2023-05-09 18:05:57 +0200

    add router

--- movie-tinder/src/components/Buttons/HeaderButton/HeaderButton.tsx
+++ movie-tinder/src/components/Buttons/HeaderButton/HeaderButton.tsx
@@ -3,22 +3,21 @@ import { Button } from "./style";
 import Grid from "@mui/material/Grid";
 import { Typography } from "@mui/material";
 import { icons } from "../../../static/icons";
+import { Link } from "react-router-dom";
 
-interface IHeaderButton {
-  onClick: () => void; 
-}
-
-export const HeaderButton: React.FC<IHeaderButton> = ({ onClick }) => {
+export const HeaderButton = () => {
   return (
-    <Button onClick={onClick}>
-      <Grid container alignItems="center">
-        <Grid item>
-          <Typography>My profile</Typography>
+    <Link to="/myProfile">
+      <Button>
+        <Grid container alignItems="center">
+          <Grid item>
+            <Typography>My profile</Typography>
+          </Grid>
+          <Grid item marginLeft={"8px"}>
+            <img src={icons.myProfileIcon} alt="my_profile" width={60} />
+          </Grid>
         </Grid>
-        <Grid item marginLeft={"8px"}>
-          <img src={icons.myProfileIcon} alt="my_profile" width={60} />
-        </Grid>
-      </Grid>
-    </Button>
+      </Button>
+    </Link>
   );
 };
--- movie-tinder/src/components/Header/Header.tsx
+++ movie-tinder/src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import { Grid, Typography, useMediaQuery } from "@mui/material";
 import { icons } from "../../static/icons";
 import { HeaderButton } from "../Buttons/HeaderButton/HeaderButton";
 import { typography, theme } from "../../static/theme";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   const smMedia = useMediaQuery(theme.breakpoints.down("sm"));
@@ -14,30 +15,28 @@ export const Header = () => {
       width="100%"
       paddingY={smMedia ? "20px" : "40px"}
     >
-      <Grid item>
-        <Grid container alignItems="center">
-          <Grid item>
-            <img src={icons.headerIcon} alt="header_logo" />
-          </Grid>
-          <Grid item>
-            <Typography
-              style={
-                smMedia
-                  ? { ...typography.heading6 }
-                  : { ...typography.heading4 }
-              }
-            >
-              MovieTinder
-            </Typography>
+      <Link to="/">
+        <Grid item>
+          <Grid container alignItems="center">
+            <Grid item>
+              <img src={icons.headerIcon} alt="header_logo" />
+            </Grid>
+            <Grid item>
+              <Typography
+                style={
+                  smMedia
+                    ? { ...typography.heading6 }
+                    : { ...typography.heading4 }
+                }
+              >
+                MovieTinder
+              </Typography>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
+      </Link>
       <Grid item>
-        <HeaderButton
-          onClick={() => {
-            console.log("go into next Page about User"); // TODO: in future make react-route
-          }}
-        />
+        <HeaderButton />
       </Grid>
     </Grid>
   );
