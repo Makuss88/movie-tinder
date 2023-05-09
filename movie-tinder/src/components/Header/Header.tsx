bc19b121f74b74acb0fde8ea24c6144f76db4974
Makuss88 <maciborek.grzegorz@gmail.com>
2023-05-09 17:20:31 +0200

    fix Header in small responsiveness

--- movie-tinder/src/components/Header/Header.tsx
+++ movie-tinder/src/components/Header/Header.tsx
@@ -1,15 +1,18 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, useMediaQuery } from "@mui/material";
 import { icons } from "../../static/icons";
 import { HeaderButton } from "../Buttons/HeaderButton/HeaderButton";
-import { typography } from "../../static/theme";
+import { typography, theme } from "../../static/theme";
 
 export const Header = () => {
+  const smMedia = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
     <Grid
       container
       justifyContent="space-between"
       alignItems="center"
-      paddingY="40px"
+      width="100%"
+      paddingY={smMedia ? "20px" : "40px"}
     >
       <Grid item>
         <Grid container alignItems="center">
@@ -17,7 +20,13 @@ export const Header = () => {
             <img src={icons.headerIcon} alt="header_logo" />
           </Grid>
           <Grid item>
-            <Typography style={{ ...typography.heading4 }}>
+            <Typography
+              style={
+                smMedia
+                  ? { ...typography.heading6 }
+                  : { ...typography.heading4 }
+              }
+            >
               MovieTinder
             </Typography>
           </Grid>
